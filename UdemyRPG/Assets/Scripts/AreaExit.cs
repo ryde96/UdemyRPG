bc87e781d0fcfed83585c6a105f4016af6e42d37
Andreas Ryde <andreas.ryde@gmail.com>
2018-11-01 19:53:57 +0100

    first commit

---
+++ UdemyRPG/Assets/Scripts/AreaExit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+
+	public string areaToLoad;
+	public string areaTransitionName;
+
+	public AreaEntrance theEntrance;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		theEntrance.transitionName = areaTransitionName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			SceneManager.LoadScene(areaToLoad);
+			PlayerController.instance.areaTransitionName = areaTransitionName;
+		}
+	}
+}
---
+++ UdemyRPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	// Public variables
+	public Rigidbody2D theRB;
+	public float moveSpeed;
+
+	public Animator playerAnimator;
+
+	public string areaTransitionName;
+
+	public static PlayerController instance;
+	
+	// Private variables
+	
+	// Use this for initialization
+	void Start ()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+		
+		
+		DontDestroyOnLoad(gameObject);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Enable us to move the player with "WASD" or arrows
+		theRB.velocity = new Vector2(
+			Input.GetAxisRaw("Horizontal"),
+			Input.GetAxisRaw("Vertical")
+			) * moveSpeed;
+		
+		playerAnimator.SetFloat("moveX", theRB.velocity.x);
+		playerAnimator.SetFloat("moveY", theRB.velocity.y);
+
+		if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 ||
+			Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+		{
+			playerAnimator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+			playerAnimator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+		}
+	}
+	
+	
+}
